152d14024506f36a6b0c399fe67c3bd9077a6455
Czarek98 <cezary.bohdanowicz@gmail.com>
2021-04-22 00:13:08 +0200

    Add action to admin panel to accepted all unconfirmed food

--- KCALCULATOR/kcal/admin.py
+++ KCALCULATOR/kcal/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Food
 # Register your models here.
+@admin.action(description='Mark selected food as accepted')
+def make_accepted(modeladmin, request, queryset):
+    queryset.update(status='confirmed')
 @admin.register(Food)
 class FoodAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "status"]
+    actions = [make_accepted]
     pass
+
