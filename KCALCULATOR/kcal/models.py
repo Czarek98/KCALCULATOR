17e0cb17dfd44fad04e98297bfaf79797e33adae
Czarek98 <cezary.bohdanowicz@gmail.com>
2021-04-21 22:37:49 +0200

    Starting app and create first models

---
+++ KCALCULATOR/kcal/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    kcal = models.FloatField()
+    proteins = models.FloatField()
+    fat = models.FloatField()
+    carbs = models.FloatField()
+
+
+class User(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    kcalperday = models.FloatField()
