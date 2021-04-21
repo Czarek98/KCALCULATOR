38ae239230b5aade576471bf57b58ba7a0b97ab9
Czarek98 <cezary.bohdanowicz@gmail.com>
2021-04-22 00:01:25 +0200

    Add status to accepted added food Add grams for food Better display for admin panel

--- KCALCULATOR/kcal/models.py
+++ KCALCULATOR/kcal/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+IsAccepted_STATUS = (
+    (
+        ('unconfirmed', 'Waiting for confirmed'),
+        ('confirmed', 'Accepted'),
+        ('rejected', 'Rejected')
+    )
+)
 
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=255)
+    grams = models.FloatField(default=100.0)
     kcal = models.FloatField()
-    proteins = models.FloatField()
-    fat = models.FloatField()
-    carbs = models.FloatField()
+    proteins = models.FloatField(null=True)
+    fat = models.FloatField(null=True)
+    carbs = models.FloatField(null=True)
+    status = models.CharField(choices=IsAccepted_STATUS, default='unconfirmed', max_length=40)
 
 
 class User(models.Model):
